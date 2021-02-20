033b8a5ff7164fe6215529ab37576e097e50d21b
Suji Kim <sjkim3242@gmail.com>
2021-02-20 12:50:09 -0800

    first commit

---
+++ MiMORNI-iOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MiMORNI-iOS
+//
+//  Created by Suji Kim on 2/20/21.
+//  Copyright © 2021 sjkim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
